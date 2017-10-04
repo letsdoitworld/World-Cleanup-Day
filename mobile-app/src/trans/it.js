f5bd552fea583d9575013b13973f78a2dd0f237c
Kris Haamer <kris@mindworks.ee>
2017-10-04 17:20:43 +0300

    New translations en.json (Italian)

--- mobile-app/src/trans/it.js
+++ mobile-app/src/trans/it.js
@@ -1,7 +1,7 @@
 {
   "label_text_app_subtitle":"Let's clean up the world together!",
-  "label_button_facebook":"Continue with Facebook",
-  "label_button_google":"Continue with Google",
+  "label_button_facebook":"Continua con Facebook",
+  "label_button_google":"Continua con Google",
   "label_button_try_app":"Try out the app without account",
   "label_button_acknowledge":"Ok, got it!",
   "label_button_cancel":"Cancel",

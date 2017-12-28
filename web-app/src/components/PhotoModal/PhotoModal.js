7912558c603b06c15f10c397f30b4b9a9ad9f918
Mihai Saru <mihai.saru@qualitance.com>
2017-12-28 15:32:37 +0200

    Add styling to PhotoModal (limit photo size on screen)

--- web-app/src/components/PhotoModal/PhotoModal.js
+++ web-app/src/components/PhotoModal/PhotoModal.js
@@ -3,13 +3,16 @@ import Modal from 'react-modal';
 
 import closeButton from '../../assets/closeButton.png';
 import { SHARED_MODAL_STYLES } from '../../shared/constants';
+import "./PhotoModal.css";
 
 const PhotoModal = ({ onClose, photoUrl, isOpen }) => (
   <Modal isOpen={isOpen} style={SHARED_MODAL_STYLES} contentLabel="">
     <button className="Shared-modal-close-button" onClick={onClose}>
       <img src={closeButton} alt="" />
     </button>
-    <img src={photoUrl} />
+    <div className="content">
+      <img src={photoUrl} />
+    </div>
   </Modal>
 );
 

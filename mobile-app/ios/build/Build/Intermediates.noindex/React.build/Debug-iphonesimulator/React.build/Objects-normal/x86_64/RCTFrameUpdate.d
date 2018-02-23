b646811fba40e8588f9965c6e2df7dfc5eae8903
Alexey Chernenko <alexey.chernenko@yalantis.com>
2018-02-23 18:24:36 +0200

    fixed ios

--- mobile-app/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTFrameUpdate.d
+++ mobile-app/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTFrameUpdate.d
@@ -1,6 +1,4 @@
 dependencies: \
-  /Users/ihorkucherenko/IdeaProjects/World-Cleanup-Day/mobile-app/node_modules/react-native/React/Base/RCTFrameUpdate.m \
-  /Users/ihorkucherenko/IdeaProjects/World-Cleanup-Day/mobile-app/node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  /Users/ihorkucherenko/IdeaProjects/World-Cleanup-Day/mobile-app/node_modules/react-native/React/Base/RCTUtils.h \
-  /Users/ihorkucherenko/IdeaProjects/World-Cleanup-Day/mobile-app/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/ihorkucherenko/IdeaProjects/World-Cleanup-Day/mobile-app/node_modules/react-native/React/Base/RCTDefines.h
+  /Users/saionara1/IdeaProjects/clean-app-world/mobile-app/node_modules/react-native/React/Base/RCTFrameUpdate.m \
+  /Users/saionara1/IdeaProjects/clean-app-world/mobile-app/node_modules/react-native/React/Base/RCTFrameUpdate.h \
+  /Users/saionara1/IdeaProjects/clean-app-world/mobile-app/node_modules/react-native/React/Base/RCTUtils.h

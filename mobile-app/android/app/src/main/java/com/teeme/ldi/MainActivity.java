386e83e3f442c21b1159a693e9245c7c1ec4f467
Alexey Chernenko <alexey.chernenko@yalantis.com>
2018-02-26 13:36:16 +0200

    added fabric

--- mobile-app/android/app/src/main/java/com/teeme/ldi/MainActivity.java
+++ mobile-app/android/app/src/main/java/com/teeme/ldi/MainActivity.java
@@ -1,8 +1,13 @@
 package com.teeme.ldi;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.crashlytics.android.Crashlytics;
 import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends SplashActivity {
+import io.fabric.sdk.android.Fabric;
 
+public class MainActivity extends SplashActivity {
 
 }
--- mobile-app/android/app/src/main/java/com/teeme/ldi/MainApplication.java
+++ mobile-app/android/app/src/main/java/com/teeme/ldi/MainApplication.java
@@ -1,38 +1,46 @@
 package com.teeme.ldi;
 
+import com.airbnb.android.react.maps.MapsPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.teeme.ldi.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;
 
+import io.fabric.sdk.android.Fabric;
+
 public class MainApplication extends NavigationApplication {
 
-  @Override
-  public boolean isDebug() {
-    // Make sure you are using BuildConfig from your own application
-    return BuildConfig.DEBUG;
-  }
+    @Override
+    public boolean isDebug() {
+        // Make sure you are using BuildConfig from your own application
+        return false;
+    }
 
-  protected List<ReactPackage> getPackages() {
-    // Add additional packages you require here
-    // No need to add RnnPackage and MainReactPackage
-    return Arrays.<ReactPackage>asList(
-            new ReactNativeLocalizationPackage(),
-            new MapsPackage()
-            // eg. new VectorIconsPackage()
-    );
-  }
+    protected List<ReactPackage> getPackages() {
+        // Add additional packages you require here
+        // No need to add RnnPackage and MainReactPackage
+        return Arrays.<ReactPackage>asList(
+                new ReactNativeLocalizationPackage(),
+                new MapsPackage()
+                // eg. new VectorIconsPackage()
+        );
+    }
 
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-    return getPackages();
-  }
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return getPackages();
+    }
 
-//  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
+
+    //  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 //    @Override
 //    public boolean getUseDeveloperSupport() {
 //      return BuildConfig.DEBUG;

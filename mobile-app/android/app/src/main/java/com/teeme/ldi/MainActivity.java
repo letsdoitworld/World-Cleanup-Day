38678e23b03d718cc7bb2aeca15a849937022d88
Ihor Kucherenko <kuchernkoigor@gmail.com>
2018-02-27 17:49:32 +0200

    fixed fb login for android

--- mobile-app/android/app/src/main/java/com/teeme/ldi/MainActivity.java
+++ mobile-app/android/app/src/main/java/com/teeme/ldi/MainActivity.java
@@ -1,16 +1,8 @@
 package com.teeme.ldi;
 
-import android.content.Intent;
-
 import com.reactnativenavigation.controllers.SplashActivity;
 
 public class MainActivity extends SplashActivity {
 
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-    }
 
 }

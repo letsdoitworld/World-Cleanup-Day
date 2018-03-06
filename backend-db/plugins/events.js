266c361e89b5f9f476d678c34971e909aa69ef0b
George <yalantisgeorge@gmail.com>
2018-03-06 09:39:25 +0200

    tmp

--- backend-db/plugins/events.js
+++ backend-db/plugins/events.js
@@ -13,8 +13,23 @@ module.exports = function () {
       db.ready().then(() => next()).catch(e => next(e))
   })
   lucius.register('role:db,cmd:createEvent', async function (connector, args, __) {
+    console.log('123test', args)
     return connector
+    // verify that the dataset exists
+    .request('role:db,cmd:getDatasetById', {id: args.event.datasetId})
+    .set('dataset')
+    // check in which areas the trashpoint falls
+    .input(args.event)
+    .input(event => ({
+        longitude: event.location.longitude,
+        latitude: event.location.latitude,
+    }))
+    .request('role:geo,cmd:resolveLocation')
+    .set('areas')
+    // create the event
+    .get(['dataset', 'areas'])
     .use(async function ({areas, dataset}, responder) {
+        args.event.areas = areas;
         return responder.success(await db.createEvent(dataset.id, __.user.id, args.event));
     });
   })

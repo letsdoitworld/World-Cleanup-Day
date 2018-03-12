2c416fb103dbce4c1203c4b34e5f2dffad5c8076
front <valeriy.sheremet@yalantis.com>
2018-03-12 13:38:04 +0200

    Added missing icons

--- web-app/src/pages/EventsList/Event/Event.js
+++ web-app/src/pages/EventsList/Event/Event.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+  LocationIcon24px,
+  GroupIcon24px,
+} from '../../../components/common/Icons';
 import './Event.css';
 
 export class Event extends Component {
@@ -22,9 +26,15 @@ export class Event extends Component {
           <div className="Event-details">
             <div className="Event-details-part1">
               <p className="Event-title Event-info">{title}</p>
-              <p className="Event-creator Event-info">{author}</p>
+              <p className="Event-creator Event-info">
+                <GroupIcon24px />
+                <span>{author}</span>
+              </p>
               <p className="Event-status Event-info">Upcoming</p>
-              <p className="Event-location Event-info">{location}</p>
+              <p className="Event-location Event-info">
+                <LocationIcon24px />
+                <span>{location}</span>
+              </p>
             </div>
             <div className="Event-details-part2">
               <p className="Event-fill Event-info">

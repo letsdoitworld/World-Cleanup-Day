b2d8b92a12d29c191fc027348d490c9306a2460c
Andrei Abozau <d3wayorg@gmail.com>
2018-03-22 09:45:39 +0200

    Fixed the problem of doubling the trash points created in offline mode

--- mobile-app/src/services/Offline.js
+++ mobile-app/src/services/Offline.js
@@ -8,7 +8,14 @@ const offlineDB = SQLite.openDatabase('db.offline');
 class OfflineService {
 
   constructor() {
-    this.executeSql('CREATE TABLE IF NOT EXISTS trashpoints (id integer primary key not null, url text, marker text, photos blob, dphotos blob);');
+    this.executeSql('CREATE TABLE IF NOT EXISTS trashpoints (' +
+      'id INTEGER PRIMARY KEY AUTOINCREMENT,' +
+      'url TEXT,' +
+      'marker TEXT,' +
+      'photos BLOB,' +
+      'dphotos BLOB,' +
+      'status INTEGER DEFAULT 0' +
+    ');');
   }
 
   executeSql = async (sql, params = []) => {
@@ -25,7 +32,7 @@ class OfflineService {
     if (!newMarker.name || newMarker.name.length < 1) {
       newMarker.name = `${latitude.toFixed(2)}, ${longitude.toFixed(2)}`;
     }
-    await this.executeSql('INSERT INTO trashpoints (url, marker, photos, dphotos) VALUES (?, ?, ?, ?);', [
+    await this.executeSql('INSERT OR REPLACE INTO trashpoints (url, marker, photos, dphotos) VALUES (?, ?, ?, ?);', [
       url,
       JSON.stringify(newMarker),
       JSON.stringify(photos),
@@ -38,47 +45,57 @@ class OfflineService {
 
   async syncToServer() {
     const transaction = await offlineDB.transaction(tx => {
-      tx.executeSql('SELECT * FROM trashpoints', [], async (_, { rows: { _array } }) => {
-
+      const idsToPush = [];
+      tx.executeSql('SELECT id FROM trashpoints WHERE status = 0', [], async (_, { rows: { _array } }) => {
         for (let i = 0; i < _array.length; i++) {
-          let dataOk = true;
-          let trashpoint = {};
-          try {
-            trashpoint = {id: _array[i].id, url: _array[i].url};
-            trashpoint.marker = JSON.parse(_array[i].marker);
-            trashpoint.photos = JSON.parse(_array[i].photos);
-            trashpoint.dphotos = JSON.parse(_array[i].dphotos);
-          } catch (e) {
-            dataOk = false;
-          }
-          if (dataOk) {
-            const createMarkerResponse = await Api.put(trashpoint.url, trashpoint.marker);
-            if (createMarkerResponse) {
-              offlineDB.transaction(tx => {
-                tx.executeSql('DELETE FROM trashpoints WHERE id = ?;', [trashpoint.id]);
-              });
-
-              if (trashpoint.photos.length > 0) {
-                await handleUpload({
-                  photos: trashpoint.photos,
-                  markerId: createMarkerResponse.data.id
-                });
-              }
-
-              if (trashpoint.dphotos.length > 0) {
-                try {
-                  await Promise.all(
-                    trashpoint.dphotos.map(p => deleteImage(id, p.parentId)),
-                  );
-                } catch (ex) {
-                  console.log(ex);
-                }
-              }
-
-            }
-          }
+          idsToPush.push(_array[i].id);
         }
       });
+      if (idsToPush.length > 0) {
+        const whereIds = idsToPush.join(',');
+        tx.executeSql('UPDATE trashpoints SET status = 1 WHERE id IN (?)', [whereIds]);
+        tx.executeSql('SELECT id, url, marker, photos, dphotos FROM trashpoints WHERE id IN (?)', [whereIds], async (_, { rows: { _array } }) => {
+          for (let i = 0; i < _array.length; i++) {
+            let dataOk = true;
+            let trashpoint = {};
+            try {
+              trashpoint = {id: _array[i].id, url: _array[i].url};
+              trashpoint.marker = JSON.parse(_array[i].marker);
+              trashpoint.photos = JSON.parse(_array[i].photos);
+              trashpoint.dphotos = JSON.parse(_array[i].dphotos);
+            } catch (e) {
+              dataOk = false;
+            }
+            if (dataOk) {
+              const createMarkerResponse = await Api.put(trashpoint.url, trashpoint.marker);
+              if (createMarkerResponse) {
+                offlineDB.transaction(tx => {
+                  tx.executeSql('DELETE FROM trashpoints WHERE id = ?;', [trashpoint.id]);
+                });
+                if (trashpoint.photos.length > 0) {
+                  await handleUpload({
+                    photos: trashpoint.photos,
+                    markerId: createMarkerResponse.data.id
+                  });
+                }
+                if (trashpoint.dphotos.length > 0) {
+                  try {
+                    await Promise.all(
+                      trashpoint.dphotos.map(p => deleteImage(id, p.parentId)),
+                    );
+                  } catch (ex) {
+                    // console.log(ex);
+                  }
+                }
+              } else {
+                offlineDB.transaction(tx => {
+                  tx.executeSql('UPDATE trashpoints SET status = 0 WHERE id = ?;', [trashpoint.id]);
+                });
+              }
+            }
+          }
+        });
+      }
     });
   }
 }

6c02a778dfd0419059e0d4f9646408e68229d9ba
anna <anna.severinovskaya@yalantis.com>
2018-03-29 15:37:04 +0300

    Fix crash and some issues with list

---
+++ mobile-app/src/screens/TrashPoints/index.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {fetchUserLocation} from '../../store/actions/locations';
+
+import {getUserCountry,} from '../../store/selectors';
+
+import Component from './TrashPoints';
+
+const selector = createStructuredSelector({
+  country: getUserCountry,
+});
+
+const actions = {
+  onFetchLocation: fetchUserLocation,
+};
+
+export default connect(selector, actions)(Component);

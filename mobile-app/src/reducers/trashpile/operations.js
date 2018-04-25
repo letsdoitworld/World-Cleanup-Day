ed9f2e0bc61fca078d5d4537dd3f6164e4480b3c
kakarda653 <Noorsoft2015>
2018-04-25 15:08:14 +0300

    fix offline trashpoint create

--- mobile-app/src/reducers/trashpile/operations.js
+++ mobile-app/src/reducers/trashpile/operations.js
@@ -323,6 +323,7 @@ export const createMarker = ({
           photos,
           !isEdit ? [] : toDeletePhotos
         );
+        dispatch(appOps.updateSyncStatus(false))
       }
 
       if (!createMarkerResponse) {

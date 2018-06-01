26d47a2fb0a45ad87928a76842289d792d966d07
vladimir <vladimir.zhuravlev@yalantis.com>
2018-06-01 12:20:12 +0300

    Edit code style

--- backend-db/plugins/events.js
+++ backend-db/plugins/events.js
@@ -171,16 +171,14 @@ module.exports = function () {
                   }
                 }
                 event.trashpoints = filteredTrashpoints;
-                if (event.offlineAttendeesAmount <= event.maxPeopleAmount) {
-                    const savedEvent = await db.createEvent(__.user.id, event);
-                    const mappedEvent = await mapEvent(savedEvent);
-                    //TODO Status TRUE should be implemented all over the project. For now it's just mock data
-                    mappedEvent.status = true;
-                    return responder.success(mappedEvent);
-                }
                 if (event.offlineAttendeesAmount > event.maxPeopleAmount) {
                     return responder.failure(new LuciusError(E.OFFLINE_ATTENDEES_AMOUNT));
                 }
+                const savedEvent = await db.createEvent(__.user.id, event);
+                const mappedEvent = await mapEvent(savedEvent);
+                //TODO Status TRUE should be implemented all over the project. For now it's just mock data
+                mappedEvent.status = true;
+                return responder.success(mappedEvent);
             });
     });
     lucius.register('role:db,cmd:getEvents', async function (connector, args) {

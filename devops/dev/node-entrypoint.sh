9cd01733b6484dc0bd1429d9c14b670f66976e0a
vladimir <vladimir.zhuravlev@yalantis.com>
2018-06-08 12:12:46 +0300

    reset to default docker settings

--- devops/dev/node-entrypoint.sh
+++ devops/dev/node-entrypoint.sh
@@ -10,17 +10,17 @@ if [ ! -f "$INIT_FLAG" ]; then
     cd /src && sudo rm -rf node_modules
 
     for MOD in lucius logger util service-factory; do
-        cd /module-${MOD} && sudo yarn link
-        cd /src && sudo yarn link "module-${MOD}"
+        cd /module-${MOD} && yarn link
+        cd /src && yarn link "module-${MOD}"
     done
 
-    cd /module-util  && sudo yarn
-    cd /module-logger  && sudo yarn
-    cd /module-lucius  && sudo yarn link module-logger module-util && sudo yarn
-    cd /module-service-factory  && sudo yarn link module-logger && sudo yarn
+    cd /module-util && rm -rf node_modules && yarn
+    cd /module-logger && rm -rf node_modules && yarn
+    cd /module-lucius && rm -rf node_modules && yarn link module-logger module-util && yarn
+    cd /module-service-factory && rm -rf node_modules && yarn link module-logger && yarn
 
     (while true; do echo "...installing packages"; sleep 5; done) & PROGRESS_PID=$!
-    cd /src && sudo yarn
+    cd /src && yarn
     kill "$PROGRESS_PID" &>/dev/null
 
     echo "<<< initialization done"

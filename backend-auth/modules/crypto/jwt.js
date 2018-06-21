ddbe47c489e1afde5a2109080b27a8e10a822df8
vladimir.zhuravlev@yalantis.com <vladimir.zhuravlev@yalantis.com>
2018-06-21 16:36:18 +0300

    add jwt expiration date

--- backend-auth/modules/crypto/jwt.js
+++ backend-auth/modules/crypto/jwt.js
@@ -3,11 +3,12 @@ const jwt = require('jsonwebtoken');
 const logger = require('module-logger');
 
 const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRATION = {expiresIn: '90d'};
 
 module.exports = {
     create: content => {
         try {
-            return jwt.sign(content, JWT_SECRET);
+            return jwt.sign(content, JWT_SECRET, JWT_EXPIRATION);
         } catch (e) {
             logger.fatal(e);
             return false;

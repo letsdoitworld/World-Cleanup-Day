8f5b50a313d6ea6b1c9c86c70514b655b7de2669
Dmitriy Zolotukhin <kakarda653@gmail.com>
2018-06-28 16:28:12 +0300

    offline actions

--- mobile-app/src/store/reducers/app.js
+++ mobile-app/src/store/reducers/app.js
@@ -1,6 +1,11 @@
 import Immutable from 'immutable';
 
-import { PROGRESS_ACTION } from '../types/app';
+import {
+  PROGRESS_ACTION,
+  SET_CONNECTION_CHECKED,
+  UPDATE_NETWORK_STATUS,
+  UPDATE_LACK_CONNECTION_MESSAGE_STATUS, UPDATE_SYNC_STATUS,
+} from '../types/app';
 import {
   FETCH_DATASETS_SUCCESS,
 } from '../actions/app';
@@ -13,6 +18,10 @@ export const initialState = Immutable.Map(
     progress: undefined,
     datasetUUID: undefined,
     error: undefined,
+    isConnected: false,
+    connectionChecked: false,
+    noLackConnectionAlert: false,
+    inSync: false,
   });
 
 const handlers = {
@@ -26,6 +35,26 @@ const handlers = {
       .set('datasetUUID', payload),
     );
   },
+  [SET_CONNECTION_CHECKED]: (state) => {
+    return state.withMutations(mState => mState
+      .set('connectionChecked', true)
+    );
+  },
+  [UPDATE_NETWORK_STATUS]: (state, { payload }) => {
+    return state.withMutations(mState => mState
+      .set('isConnected', payload.isConnected)
+    );
+  },
+  [UPDATE_SYNC_STATUS]: (state, { payload }) => {
+    return state.withMutations(mState => mState
+      .set('inSync', payload.inSync)
+    );
+  },
+  [UPDATE_LACK_CONNECTION_MESSAGE_STATUS]: (state, { payload }) => {
+    return state.withMutations(mState => mState
+      .set('noLackConnectionAlert', payload.noLackConnectionAlert)
+    )
+  }
 };
 
 export default createReducer(initialState, handlers);

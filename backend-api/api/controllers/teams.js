c526d65094b92531018faab0253072b47d0dfbef
slava.p <slava.p@techery.io>
2018-07-05 14:04:47 +0300

    Add request for getting team list for web app

--- backend-api/api/controllers/teams.js
+++ backend-api/api/controllers/teams.js
@@ -2,17 +2,35 @@
 const senecaRequestMw = require('../../modules/swagger-setup/middleware/seneca');
 
 module.exports = {
-  getAllTeams: senecaRequestMw(
-    'role:db,cmd:getAllTeams',
-    req => ({
-      CC: req.swagger.params.country.value,
-      search: req.swagger.params.search.value,
-    }),
-  ),
-  getTeam: senecaRequestMw(
-    'role:db,cmd:getTeam',
-    req => ({
-      id: req.swagger.params.id.value,
-    }),
-  )
+    getAllTeams: senecaRequestMw(
+        'role:db,cmd:getAllTeams',
+        req => ({
+            CC: req.swagger.params.country.value,
+            search: req.swagger.params.search.value,
+        }),
+    ),
+    getTeamsInBetween: senecaRequestMw(
+        'role:db,cmd:getTeamsInBetween',
+        req => ({
+          from: req.swagger.params.from.value,
+          to: req.swagger.params.to.value,
+          nameTeam: req.swagger.params.nameTeam.value,
+        }),
+    ),
+    getAllTeamsWeb: senecaRequestMw(
+        'role:db,cmd:getAllTeamsWeb',
+        req => ({
+            CC: req.swagger.params.country.value,
+            superadmin: req.swagger.params.superadmin.value,
+        }),
+    ),
+    getCountTeamsTrashpoints: senecaRequestMw(
+        'role:db,cmd:getCountTeamsTrashpoints',
+    ),
+    getTeam: senecaRequestMw(
+        'role:db,cmd:getTeam',
+        req => ({
+            id: req.swagger.params.id.value,
+        }),
+    )
 };

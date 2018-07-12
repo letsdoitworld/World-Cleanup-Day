07b66a40f8d2678982399aaf7ffe482801714209
front <valeriy.sheremet@yalantis.com>
2018-07-12 12:08:35 +0300

    Pull request #162 github. Works nice

--- web-app/src/services/Image.js
+++ web-app/src/services/Image.js
@@ -1,3 +1,5 @@
+const EXIF = require('exif-js');
+
 const ImageService = {
   async getBase64(file) {
     return new Promise((resolve, reject) => {
@@ -75,6 +77,52 @@ const ImageService = {
       };
       image.src = `data:image/jpg;base64,${base64}`;
     });
+  },
+
+  async getEXIFCoordinates(base64) {
+    const image = new Image();
+    image.src = `data:image/jpg;base64,${base64}`;
+
+    // convert to DDD coordinate format as used by the Geolocation API
+    function sanitizeEXIFCoordinate(coords, ref) {
+      if (coords === undefined) return '';
+
+      // resolve possible fractions as per spec (eg. stores [dd/1, mmmm/100, 0/1] for DMM format) and aggregate
+      let coordinate = coords.reduce((sum, val, idx) => {
+        return sum + (val.numerator / val.denominator) / Math.pow(60, idx)
+      }, 0);
+
+      // account for coordinate quadrant
+      if (ref === 'S' || ref === 'W') {
+        coordinate = -coordinate;
+      }
+
+      return coordinate;
+    }
+
+    function extractCoordinates() {
+      EXIF.getData(image, function() {
+        let lat = EXIF.getTag(this, 'GPSLatitude');
+        let ref = EXIF.getTag(this, 'GPSLatitudeRef');
+        lat = sanitizeEXIFCoordinate(lat, ref);
+
+        let lng = EXIF.getTag(this, 'GPSLongitude');
+        ref = EXIF.getTag(this, 'GPSLongitudeRef');
+        lng = sanitizeEXIFCoordinate(lng, ref);
+        console.log(lat + " | " + lng); // TODO: remove when verified
+        image.lat = lat;
+        image.lng = lng;
+      });
+    }
+
+    function load() {
+      return new Promise(function (resolve, reject) {
+        image.onload = resolve;
+      });
+    }
+
+    await load().then(extractCoordinates);
+    return { lat: image.lat, lng: image.lng };
   },
 };
 

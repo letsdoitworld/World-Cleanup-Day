2fcb629bbea027e9a932d3e11fccb75ed05eb2c9
Jaka Kranjc <smgl@lynxlynx.info>
2018-07-23 08:02:52 +0200

    i18n: fixed the expected lang file names and removed dupes

--- mobile-app/src/assets/strings.js
+++ mobile-app/src/assets/strings.js
@@ -1,35 +1,31 @@
 /* eslint-disable max-len,no-trailing-spaces */
 import LocalizedStrings from 'react-native-localization';
-import EN from './translations/en.json';
-import BE from './translations/be.json';
-import CEB from './translations/ceb.json';
-import EL from './translations/el.json';
-import ES from './translations/es.json';
-import ET from './translations/et.json';
-import FIL from './translations/fil.json';
-import FO from './translations/fo.json';
-import HI from './translations/hi.json';
-import HU from './translations/hu.json';
-import ID from './translations/id.json';
-import IT from './translations/it.json';
-import ITA from './translations/ita.json';
-import JA from './translations/ja.json';
-import NL from './translations/nl.json';
-import FAO from './translations/fao.json';
-import NO from './translations/no.json';
-import NOR from './translations/nor.json';
-import PL from './translations/pl.json';
-import PT from './translations/pt.json';
-import RO from './translations/ro.json';
-import RUS from './translations/rus.json';
-import SL from './translations/sl.json';
-import SQ from './translations/sq.json';
-import TL from './translations/tl.json';
-import TR from './translations/tr.json';
-import UK from './translations/uk.json';
-import UKR from './translations/ukr.json';
-import VI from './translations/vi.json';
-import ZH from './translations/zh.json';
+import EN from './translations/en_US.json';
+import BE from './translations/be_BY.json';
+import CEB from './translations/ceb_PH.json';
+import EL from './translations/el_GR.json';
+import ES from './translations/es_ES.json';
+import ET from './translations/et_EE.json';
+import FIL from './translations/fil_PH.json';
+import FO from './translations/fo_FO.json';
+import HI from './translations/hi_IN.json';
+import HU from './translations/hu_HU.json';
+import ID from './translations/id_ID.json';
+import IT from './translations/it_IT.json';
+import JA from './translations/ja_JP.json';
+import NL from './translations/nl_NL.json';
+import NO from './translations/no_NO.json';
+import PL from './translations/pl_PL.json';
+import PT from './translations/pt_PT.json';
+import RO from './translations/ro_RO.json';
+import RU from './translations/ru_RU.json';
+import SL from './translations/sl_SI.json';
+import SQ from './translations/sq_AL.json';
+import TL from './translations/tl_PH.json';
+import TR from './translations/tr_TR.json';
+import UK from './translations/uk_UA.json';
+import VI from './translations/vi_VN.json';
+import ZH from './translations/zh_CN.json';
 
 module.exports = new LocalizedStrings({
   en: EN,
@@ -38,28 +34,24 @@ module.exports = new LocalizedStrings({
   el: EL,
   es: ES,
   et: ET,
-  fao: FAO,
   fil: FIL,
   fo: FO,
   hi: HI,
   hu: HU,
   id: ID,
   it: IT,
-  ita: ITA,
   ja: JA,
   nl: NL,
   no: NO,
-  nor: NOR,
   pl: PL,
   pt: PT,
   ro: RO,
-  rus: RUS,
+  ru: RU,
   sl: SL,
   sq: SQ,
   tl: TL,
   tr: TR,
   uk: UK,
-  ukr: UKR,
   vi: VI,
   zh: ZH,
 });
